ef1396460bc38c943319add623d9c65d21bacff4
Michał Łukasiewicz <michal.lukasiewicz@gmail.com>
2015-04-04 18:05:25 +0200

    gyp.rb

---
+++ gyp.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Gyp < Formula
+
+      # homebrew should automatically know what to do svn repos
+      url "https://github.com/mgamer/gyp.git"
+      version "1.0"
+
+      depends_on :python
+
+      # initialize the installation of this
+      def install
+       
+        # use the brew python to install the tools needed
+	      system 'python', 'setup.py', 'install'
+
+        # install the gyp executable
+        bin.install("gyp")
+        bin.install("gyp_main.py")
+        
+     end
+end
