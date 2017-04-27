d745a4304f91c0fa08708f04854128d2d3ebee3e
Michał Łukasiewicz <michal.lukasiewicz@gmail.com>
2017-04-27 11:23:31 +0200

    Update key4cert.rb

--- key4cert.rb
+++ key4cert.rb
@@ -1,6 +1,6 @@
 class Key4cert < Formula
   homepage "http://brightinventions.pl/"
-  url "https://github.com/bright/key4cert.git", :revision => 'c733cd6a92163070023a1039aaaeb66f1ef035aa', :tag => "0.1"
+  url "https://github.com/bright/key4cert.git", :revision => '3a26b3a8eeae0c06e6a7ffd5473b18521b54beb3', :tag => "0.1.1"
 
   def install
     system "make"
